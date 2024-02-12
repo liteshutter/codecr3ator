af482564ad112c917c68a44ff9ac1ec2b33eded4
Rui Laite <122804692+liteshutter@users.noreply.github.com>
2024-02-12 09:18:39 +0200

    Update app.js

--- JS/app.js
+++ JS/app.js
@@ -5,3 +5,26 @@ menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+function sendMail(){
+    (function(){
+        emailjs.init("pMP2qNqmUOQKdN2Ke"); //account public key
+    })();
+    
+    var params = {
+        fname: document.querySelector("#name").value,
+        lname: document.querySelector("#surname").value,
+        email: document.querySelector("#email").value,
+        contact: document.querySelector("#contact").value,
+        comment: document.querySelector("#message").value,
+    };
+
+    var serviceID = "service_uynr6q8"
+    var templateID = "template_5mlualf"
+
+    emailjs.send(serviceID, templateID, params)
+    .then( res => {
+        alert("Email Sent Sucessfully!")
+    })
+    .catch();
+}
